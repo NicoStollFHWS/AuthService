cc36816c75811f7f257b43dd567f597756907205
NicoStollFHWS <92314192+NicoStollFHWS@users.noreply.github.com>
2023-01-19 17:00:59 +0100

    delete to correct commit

--- AuthService/src/main/java/com/nicolas/stoll/authservice/persistency/UserRepo.java
+++
@@ -1,6 +0,0 @@
-package com.nicolas.stoll.authservice.persistency;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepo extends CrudRepository<User, String> {
-}
